3671ecaca4910987ff0fd5d5ac1ec886d4441bc8
shovan04 <shovanm50@gmail.com>
2023-12-01 13:29:12 +0530

    work-on-win-not-mac

--- func.class.php
+++ func.class.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 
 $devDIR = '/Applications/XAMPP/xamppfiles/htdocs/educom';
+$devDIRWin = 'D:\XAMPP\htdocs\EduCom';
 $prodDIR = __DIR__;
 
-$BASEDIR = $devDIR;
+$BASEDIR = $devDIRWin;
