433c079ccd86ce14c79c6b5e66c805404c60fc2f
shovan04 <shovanm50@gmail.com>
2023-12-01 13:42:45 +0530

    fix issue try mac

--- func.class.php
+++ func.class.php
@@ -1,6 +1,22 @@
 <?php
 
 ini_set('display_errors', 1);
+// Allow requests from specific origin
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+
+// Allow the following methods
+header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+
+// Allow the following headers
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Check if it's a preflight request (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Respond with a 200 OK status for preflight requests
+    http_response_code(200);
+    exit;
+}
+
 
 $devDIR = '/Applications/XAMPP/xamppfiles/htdocs/educom';
 $devDIRWin = 'D:\XAMPP\htdocs\EduCom';
